fe686d33fef1d6649212fdc84a66c25c9de33eea
Ahmed <ahmedl9@seas.upenn.edu>
2019-01-10 11:31:50 -0500

    added main loop and window

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #This is our main file 
+import pygame
+
+def main():   
+    pygame.init()
+    screen = pygame.display.set_mode((300, 300), 0,32)
+    clock = pygame.time.Clock()
+
+    isRunning = True
+    
+    while isRunning:
+        time_passed = clock.tick(50)
+
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                isRunning = False
+
+if __name__=="__main__":
+    main()
+                
