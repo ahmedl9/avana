98996bed13024f859b9a7d5fe0069f7f9acc0887
Varun Singhai <varunsinghai@outlook.com>
2019-01-11 03:23:15 -0500

    added boundaries to credit, balance

--- HUD.py
+++ HUD.py
@@ -2,9 +2,9 @@ import pygame
 
 class hud:    
     def __init__(self):
-        self.happiness = 0
-        self.creditScore = 0
-        self.balance = 0
+        self.happiness = 2
+        self.creditScore = 400
+        self.balance = 1000
 
     def getHapiness(self):
         return self.happiness
@@ -17,9 +17,19 @@ class hud:
 
     def changeHapiness(self, val):
         self.happiness = self.happiness + val
+        if self.happiness < 0:
+            self.happiness = 0
+        elif self.happiness > 2:
+            self.happiness = 2
+
+
 
     def changeCreditScore(self, val):
         self.creditScore = self.creditScore + val
+        if self.creditScore > 800:
+            self.creditScore = 800
+        elif self.creditScore < 0:
+            self.creditScore = 0
 
     def changeBalance(self, val):
         self.balance = self.balance + val
@@ -27,11 +37,22 @@ class hud:
     def render(self, screen):
         creditScoreImg = pygame.image.load('Assets/creditscore_bar.png')
         balanceImg = pygame.image.load('Assets/balance_bar.png')
-        happinessImg = pygame.image.load('Assets/happiness1.png')
+        happinessImg = pygame.image.load('Assets/happiness-1.png')
+        happinessImg2 = pygame.image.load('Assets/happiness-2.png')
+        happinessImg3 = pygame.image.load('Assets/happiness-3.png')
 
         screen.blit(creditScoreImg, (50, 50))
-        screen.blit(happinessImg, (384, 59))
         screen.blit(balanceImg, (500, 50))
+
+        if (self.happiness == 0):
+            screen.blit(happinessImg3, (384, 59))
+        elif (self.happiness == 1):
+            screen.blit(happinessImg2, (384, 59))
+        elif (self.happiness == 2):
+            screen.blit(happinessImg1, (384, 59))
+
+
+
 
         font = pygame.font.Font("Assets/Minecraft.ttf", 24)
         text = font.render("Credit Score: " + str(self.creditScore), True, (0, 0, 0))
